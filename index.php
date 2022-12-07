d4905f25557d1f83d40b6c7ed555b433c4295219
Marco Inchingoli <evilmarkus@hotmail.it>
2022-12-07 15:42:41 +0100

    Add Constructor

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
+/* definisci una classe ‘Movie’
+all'interno della classe sono dichiarate delle variabili d'istanza
+all'interno della classe è definito un costruttore
+all'interno della classe è definito almeno un metodo
+vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà */
 
 class Movie
 {
+    public $title;
+    public $genre;
+
+    function __construct($_title)
+    {
+        $this->title = $_title;
+    }
+
 
 }
 
