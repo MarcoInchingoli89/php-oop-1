d4618758c11495e22be2f153f06fa0520cf8030a
Marco Inchingoli <evilmarkus@hotmail.it>
2022-12-07 16:19:54 +0100

    Add Movie Objects and Print Properties

--- index.php
+++ index.php
@@ -10,17 +10,32 @@ class Movie
     public $title;
     public $genre;
 
-    function __construct($_title)
+    function __construct($_title, $_genre)
     {
         $this->title = $_title;
+        $this->genre = $_genre;
     }
     function get_title()
     {
         return $this->title;
     }
+
+    function get_genre()
+    {
+        return $this->genre;
+    }
 }
 
-
+$dragon_heart = new Movie("Dragon Heart", "Fantasy");
+/* $dragon_heart->genre = "Fantasy"; */
+echo $dragon_heart->get_title() . '<br>';
+echo $dragon_heart->get_genre() . '<br>';
+/* var_dump($dragon_heart); */
+$nightmare = new Movie("Nightmare", "Horror");
+echo $nightmare->get_title() . '<br>';
+echo $nightmare->get_genre();
+/* $nightmare->genre = "Horror"; */
+/* var_dump($nightmare); */
 
 
 
