965f6a02a375911052dd3235b4845ae205b83350
Marco Inchingoli <evilmarkus@hotmail.it>
2022-12-07 16:36:21 +0100

    Add Bonus 1

--- index.php
+++ index.php
@@ -5,12 +5,18 @@ all'interno della classe è definito un costruttore
 all'interno della classe è definito almeno un metodo
 vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà */
 
+/* Bonus 1:
+Modificare la classe Movie in modo che accetti piú di un genere.
+Bonus 2:
+Creare un layout completo per stampare a schermo una lista di movies.
+Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle. */
+
 class Movie
 {
     public $title;
     public $genre;
 
-    function __construct($_title, $_genre)
+    function __construct($_title, ...$_genre)
     {
         $this->title = $_title;
         $this->genre = $_genre;
@@ -26,12 +32,12 @@ class Movie
     }
 }
 
-$dragon_heart = new Movie("Dragon Heart", "Fantasy");
+$dragon_heart = new Movie("Dragon Heart", "Fantasy", "Action", "Adventure");
 /* $dragon_heart->genre = "Fantasy"; */
 echo $dragon_heart->get_title() . '<br>';
 echo $dragon_heart->get_genre() . '<br>';
 /* var_dump($dragon_heart); */
-$nightmare = new Movie("Nightmare", "Horror");
+$nightmare = new Movie("Nightmare", "Horror", "Supernatural", "Splatter");
 echo $nightmare->get_title() . '<br>';
 echo $nightmare->get_genre();
 /* $nightmare->genre = "Horror"; */
